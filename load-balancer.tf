5fb53a2c165c6764b8d08591c11f128aae717cb8
root <root@DESKTOP-Q6R9DMU>
2025-03-11 01:56:57 +0000

    Adding new redirect

--- load-balancer.tf
+++ load-balancer.tf
@@ -26,17 +26,17 @@ resource "aws_lb_target_group" "frontend_tg" {
   tags = merge(local.common_tags, { Name = "${local.name_prefix}-frontend-target-group" })
 }
 
-# ✅ HTTP Listener (Redirects to HTTPS)
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.tcs-alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.frontend_tg.arn
-  }
-}
+# # ✅ HTTP Listener (Redirects to HTTPS)
+# resource "aws_lb_listener" "http_listener" {
+#   load_balancer_arn = aws_lb.tcs-alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
+#
+#   default_action {
+#     type = "forward"
+#     target_group_arn = aws_lb_target_group.frontend_tg.arn
+#   }
+# }
 
 # ✅ HTTPS Listener (Frontend - Port 80)
 resource "aws_lb_listener" "https_listener_frontend" {
@@ -72,7 +72,7 @@ resource "aws_lb_target_group" "backend_tg" {
 
 #Route "/api/*" to backend target group
 resource "aws_lb_listener_rule" "backend_rule" {
-  listener_arn = aws_lb_listener.http_listener.arn
+  listener_arn = aws_lb_listener.https_listener_frontend.arn
   priority     = 100
   condition {
     path_pattern {
@@ -102,7 +102,7 @@ resource "aws_lb_target_group" "grafana" {
 }
 
 resource "aws_lb_listener_rule" "grafana" {
-  listener_arn = aws_lb_listener.http_listener.arn
+  listener_arn = aws_lb_listener.https_listener_frontend.arn
   priority     = 120
 
   condition {
